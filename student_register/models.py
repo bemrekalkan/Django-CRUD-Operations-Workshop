dcdf991d908e5bf4a9f7fcd4f1f7a6f314b9f175
bemrekalkan <bemre.kalkan@gmail.com>
2022-08-27 16:10:26 +0300

    forms added

--- student_register/models.py
+++ student_register/models.py
@@ -7,20 +7,20 @@ class Student(models.Model):
     email = models.EmailField((""), max_length=254)
 
     GENDER = (
-        {'Female', "Female"},
-        {'Male', "Male"},
-        {'Other', "Other"},
-        {'Prefer', "Prefer not to say"},
+        ('Female', "Female"),
+        ('Male', "Male"),
+        ('Other', "Other"),
+        ('Prefer', "Prefer not to say"),
     )
 
     gender = models.CharField(max_length=50, choices = GENDER)
 
     PATH = (
-        {'Full Stack', "Full Stack"},
-        {'Data Science', "Data Science"},
-        {'DevOps', "DevOps"},
-        {'AWS', "AWS"},
-        {'ITF', "ITF"},
+        ('Full Stack', "Full Stack"),
+        ('Data Science', "Data Science"),
+        ('DevOps', "DevOps"),
+        ('AWS', "AWS"),
+        ('ITF', "ITF"),
     )
 
     gender = models.CharField(max_length=50, choices = PATH)
