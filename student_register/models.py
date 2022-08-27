09eaa0c8aeecaab66a984f7c23be3ba9aba356fd
bemrekalkan <bemre.kalkan@gmail.com>
2022-08-27 15:53:05 +0300

    model created

--- student_register/models.py
+++ student_register/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    full_name = models.CharField(max_length=50)
+    number = models.IntegerField()
+    mobile = models.IntegerField()
+    email = models.EmailField((""), max_length=254)
+
+    GENDER = (
+        {'Female', "Female"},
+        {'Male', "Male"},
+        {'Other', "Other"},
+        {'Prefer', "Prefer not to say"},
+    )
+
+    gender = models.CharField(max_length=50, choices = GENDER)
+
+    PATH = (
+        {'Full Stack', "Full Stack"},
+        {'Data Science', "Data Science"},
+        {'DevOps', "DevOps"},
+        {'AWS', "AWS"},
+        {'ITF', "ITF"},
+    )
+
+    gender = models.CharField(max_length=50, choices = PATH)
+
+    def __str__(self):
+        return self.full_name
+
+
+
